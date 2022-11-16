4e32ebfb403bc7b8bed1ae6438f1efcc1372aa91
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-11-16 13:52:28 +1100

    toggle form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -89,6 +89,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCloseForm}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
