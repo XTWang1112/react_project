096fe4a4cda220c69915912b4fac8e63c2f5dd1c
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-11-16 15:03:45 +1100

    fix bug of only rendering one filled bar

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,6 +4,8 @@ import "./Chart.css";
 const Chart = (props) => {
   const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
   const totalMax = Math.max(...dataPointValues);
+  console.log(totalMax);
+
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -3,8 +3,8 @@ import Chart from "../Chart/Chart";
 const ExpensesChart = (props) => {
   const chartDataPoints = [
     {label: "Jan", value: 0},
-    {label: "Mar", value: 0},
     {label: "Feb", value: 0},
+    {label: "Mar", value: 0},
     {label: "Apr", value: 0},
     {label: "May", value: 0},
     {label: "Jun", value: 0},
